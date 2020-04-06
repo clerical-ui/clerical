93157b20903da0c79a6744390777afe6e3899cc5
Tom Manion <thomaswmanion@gmail.com>
2020-04-06 13:28:37 -0500

    chore: remove sandbox

--- clerical-sandbox/deploy.sh
+++
@@ -1,3 +0,0 @@
-export NODE_ENV=production
-npm run build
-./node_modules/.bin/s3-deploy './dist/**' --cwd './dist/' --region us-east-1 --bucket sandbox.clerical.dev
--- clerical-sandbox/src/app.config.ts
+++
@@ -1,7 +0,0 @@
-import { IClericalConfig } from '@clerical/core';
-import { clericalSandboxRoute } from './clerical-sandbox/clerical-sandbox.route';
-
-export const appConfig: IClericalConfig = {
-    defaultPath: '/',
-    routes: [clericalSandboxRoute]
-};
